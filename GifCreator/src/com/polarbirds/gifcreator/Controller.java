d0caa61e7c6854ce54cd023ce08403c3e9f703cd
Kristian Rekstad <krirek@msn.com>
2014-07-18 18:15:11 +0200

    Added methods for moving selections up and down.

--- GifCreator/src/com/polarbirds/gifcreator/Controller.java
+++ GifCreator/src/com/polarbirds/gifcreator/Controller.java
@@ -7,18 +7,22 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-public class Controller implements Initializable {
+public class Controller implements Initializable, ThreadActionCompleteListener {
 	
 //////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////Properties//////////////////////////////////
@@ -34,6 +38,16 @@ public class Controller implements Initializable {
 	private ListView<File> rightList;
 	@FXML
 	private Tooltip leftTooltip;
+	
+	/**
+	 * getValue() returns delay in 100ths of a second. From 1 to 1000 hundreds, or 0.01 to 10.0 seconds.
+	 */
+	@FXML
+	private Slider delaySlider;
+	@FXML
+	private TextField delayField;
+	@FXML
+	private Label loadingLabel;
 	
 	@FXML
 	private Button btnAdd; //FIXME Not needed
@@ -58,12 +72,14 @@ public class Controller implements Initializable {
 //GENERAL PROPERTIES
 	
 	private FileManager fm;
+	private ImageCombiner ic;
 	private Stage stage;
 	
 	private modes mode = modes.FILESELECTION;
 	
 	private String FILESELECT_LABEL = "Select files";
 	private String GENERATE_LABEL = "Adjust settings";
+	private String LOADING_LABEL = "Loading...";
 	
 
 //////////////////////////////////////////////////////////////////////////////
@@ -83,6 +99,7 @@ public class Controller implements Initializable {
 	
 	public Controller() {
 		fm = new FileManager();
+		ic = new ImageCombiner();
 	}
 	
 	/**
@@ -93,6 +110,15 @@ public class Controller implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		fm.setAllFilesList(leftList.getItems());
 		fm.setSelectedFilesList(rightList.getItems());	
+		
+		delaySlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				delaySliderChanged();				
+			}
+		});
 	}
 	
 	public void setStage(Stage stage) {
@@ -115,6 +141,7 @@ public class Controller implements Initializable {
 		if (dir != null) {
 			fieldPath.setText(dir.getAbsolutePath());
 			fm.setPath(dir);
+			fm.loadPath();
 		}
 	}
 	
@@ -151,15 +178,35 @@ public class Controller implements Initializable {
 	}
 	
 	@FXML
+	public void moveUp (Event e) {
+		int selected = rightList.getSelectionModel().getSelectedIndex();
+		boolean succeeded = fm.moveSelectionUp(selected);
+		if (succeeded) {
+			rightList.getSelectionModel().select(selected-1);
+		}
+	}
+	
+	@FXML
+	public void moveDown (Event e) {
+		int selected = rightList.getSelectionModel().getSelectedIndex();
+		boolean succeeded = fm.moveSelectionDown(selected);
+		if (succeeded) {
+			rightList.getSelectionModel().select(selected+1);
+		}
+	}
+	
+	@FXML
 	public void nextMode (Event e) {
 		
 		switch (mode) {
 			case FILESELECTION:
 				mode = modes.GENERATING;
-				generatePane.setLayoutY(0);
+				generatePane.setLayoutY(0); //It is moved to y=390 in scene builder.
 				generatePane.setVisible(true);
 				loadPane.setVisible(false);
 				title.setText(GENERATE_LABEL);
+				loadingLabel.setText(LOADING_LABEL);
+				fm.loadImages();
 				break;
 			default:
 				break;
@@ -182,6 +229,11 @@ public class Controller implements Initializable {
 	}
 	
 	@FXML
+	public void sliderReleased (Event e) {
+		ic.generate();
+	}
+	
+	@FXML
 	public void saveGif (Event e) {
 		//TODO WIP
 	}
@@ -189,13 +241,33 @@ public class Controller implements Initializable {
 /////////////////////////////////Methods/////////////////////////////////////
 //UTILITY
 	
-
+	public void delaySliderChanged() {
+		delayField.setText(
+					String.format("%.2f", delaySlider.getValue()/100)); //2 decimals, display seconds.
+	}
 	
 	public void toggleGenerateButton() {
 		if (rightList.getItems().size() == 0) {
 			btnGenerate.setDisable(true);
 		} else {
 			btnGenerate.setDisable(false);
+		}
+	}
+
+	@Override
+	public void actionComplete(ThreadActionCompleteListener.action e) {
+		switch (e) {
+		case FILES_LOADED:
+			loadingLabel.setText("");
+			ic.generate();
+			break;
+			
+		case GIF_GENERATED:
+			
+			break;
+			
+		default:
+				break;
 		}
 	}
 	
