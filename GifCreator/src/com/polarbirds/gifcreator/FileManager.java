e23226971021531b2e6b1ca0a084f7de19df6f83
Kristian Rekstad <krirek@msn.com>
2014-07-19 23:06:30 +0200

    Now loads images in a separate thread and stores them in an ArrayList<BufferedImage>.

--- GifCreator/src/com/polarbirds/gifcreator/FileManager.java
+++ GifCreator/src/com/polarbirds/gifcreator/FileManager.java
@@ -1,38 +1,46 @@
 package com.polarbirds.gifcreator;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Window;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 
 /**
  * 
  * @author Kristian
  * WIP.
  */
-public class FileManager implements Runnable {
+public class FileManager {
 	
 	private File path;
+	private final List<BufferedImage> images;
 	
 	private ObservableList<File> allFiles;
 	private ObservableList<File> selectedFiles;
 	
-	public FileManager() {}
+	private final List<ThreadActionCompleteListener> listeners;
+
+	
+	public FileManager() {
+		listeners = new ArrayList<ThreadActionCompleteListener>();
+		images = new ArrayList<BufferedImage>();
+	}
 	
 	public void setPath(File path) {
 		this.path = path;
 	}
 	
 	public File getPath() {
-		return path;
+		return path; 
 	}
 	
 	public void setAllFilesList(ObservableList<File> list) {
@@ -56,6 +64,36 @@ public class FileManager implements Runnable {
 		if (-1 < index && index < selectedFiles.size()) {
 			selectedFiles.remove(index);
 		}
+	}
+	
+	/**
+	 * @param index of the file to move.
+	 * @return true if the file was moved, false otherwise.
+	 */
+	public boolean moveSelectionUp(int index) {
+		//Valid range is [1, last file]
+		if (0 < index && index < selectedFiles.size()) {
+			File above = selectedFiles.get(index-1);
+			selectedFiles.set(index-1, selectedFiles.get(index));
+			selectedFiles.set(index, above);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * @param index of the file to move.
+	 * @return true if the file was moved, false otherwise.
+	 */
+	public boolean moveSelectionDown(int index) {
+		//Valid range is [0, last file - 1]
+		if (0 <= index && index < selectedFiles.size()-1) {
+			File below = selectedFiles.get(index+1);
+			selectedFiles.set(index+1, selectedFiles.get(index));
+			selectedFiles.set(index, below);
+			return true;
+		}
+		return false;
 	}
 	
 	public void loadPath() {
@@ -82,50 +120,82 @@ public class FileManager implements Runnable {
 		}
 	}
 	
-	/**
-	 * FIXME change the functionality. It should return File[] files from selectedFiles.
-	 * @param path
-	 * @param wind
-	 */
-	@Deprecated
-	public String[] getFiles(String path, Window wind) {
-		
-		List<String> extensions = new ArrayList<String>();
-		String[] validExtensions = ImageIO.getReaderFileSuffixes();
-		
-		for (String ext : validExtensions) {
-			extensions.add("*."+ext);
+	public void loadImages() {
+		//Make sure the loader does not rely on a mutable array.
+		final File[] files = new File[selectedFiles.size()];
+		for (int i = 0; i < selectedFiles.size(); i++) {
+			files[i] = new File(selectedFiles.get(i).getPath());
 		}
+		final BufferedImage[] temp = new BufferedImage[files.length];
 		
-		FileChooser a = new FileChooser();
-		a.setInitialDirectory(new File(path));
-		a.setTitle("Choose images");
-		a.getExtensionFilters().add(new ExtensionFilter("Image files", extensions));
+		//Create task
+		Task<Void> loadTask = new Task<Void>() {
+			
+			@Override
+			protected Void call() throws Exception {
+				int i = 0;
+				//TODO implement a way to cancel.
+				for (File image : files) {
+					try {
+						// Possible optimization: load images into a map using path as key. avoids duplicates. Assign key to various indexes
+						// in a string array. When adding images from the map to the ArrayList later, use the same BufferedImage references where
+						// duplicates occur.
+						temp[i] = ImageIO.read(image);
+					} catch (Exception e) {}
+					finally {
+						i++;
+					}
+				}
+				return null;
+			}
+		}; 
 		
-		List<File> files = a.showOpenMultipleDialog(wind);
+		//Set action to perform on success
+		//inb4 "use lamba u filthy skrub"
+		loadTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(WorkerStateEvent event) {
+				
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						images.clear();
+						for (BufferedImage image : temp) {
+							images.add(image);
+						}
+						
+						for (ThreadActionCompleteListener listener : listeners) {
+							listener.actionComplete(ThreadActionCompleteListener.action.FILES_LOADED);
+						}
+					}
+				});
+			}
+		}); //Does this even work?
 		
-		
-		String[] out = new String[files.size()];
-		
-		Iterator<File> it = files.iterator();
-		
-		int index = 0;
-		while (it.hasNext() && index < out.length) {
-			it.next();
-			out[index] = it.next().getPath();
-			index++;
-		}
-		
-		return out;
+		//Run task
+		Thread loadThread = new Thread(loadTask);
+		loadThread.setDaemon(true); //Not sure what happens on false.
+		loadThread.start();
 	}
 	
 	/**
-	 * WIP
-	 * TODO I don't know yet. Loading images should be multi threaded.
-	 * Might create a different, private class for it.
+	 * Not implemented.
 	 */
-	@Override
-	public void run() {
-		
+	public BufferedImage[] getImages() {
+		BufferedImage[] out = new BufferedImage[images.size()];
+		int i = 0;
+		for (BufferedImage image : images) {
+			out[i] = image;
+			i++;
+		}
+		return out;
+	}
+	
+	public void addListener(ThreadActionCompleteListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeListener(ThreadActionCompleteListener listener) {
+		listeners.remove(listener);
 	}
 }
