d28c14170b6b319d75e9366f2a623d8ac1279f40
Kristian Rekstad <krirek@msn.com>
2014-07-30 20:58:00 +0200

    Sets file name instead of file path as text.

---
+++ GifCreator/src/com/polarbirds/gifcreator/FileNameCell.java
@@ -0,0 +1,21 @@
+package com.polarbirds.gifcreator;
+
+import java.io.File;
+
+import javafx.scene.control.ListCell;
+
+public class FileNameCell extends ListCell<File> {
+
+	public FileNameCell() {}
+	
+	@Override
+	protected void updateItem(File item, boolean empty) {
+		super.updateItem(item, empty); //Important. Don't skip this.
+		
+		if (item == null) {
+			setText("");
+		} else {
+			setText(item.getName());
+		}
+	}
+}
